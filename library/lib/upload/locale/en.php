4a494bec29656a3d0dad39574835a143b0397dc7
Alvaro Cabrera <pateketrueke@gmail.com>
2011-08-05 21:05:04 -0500

    Add upload helper

---
+++ library/lib/upload/locale/en.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * English upload strings
+ */
+
+$lang['without_errors'] = 'Without errors';
+$lang['max_size_ini_reached'] = 'The file exceeds the size allowed in the php.ini';
+$lang['max_size_form_reached'] = 'The file exceeds the size established MAX_FILE_SIZE';
+$lang['partial_upload_error'] = 'The uploaded file was only partially and may contain errors';
+$lang['file_not_selected'] = 'No file has been selected';
+$lang['temporary_path_missing'] = 'There is no temporary folder for files';
+$lang['write_file_error'] = 'Failed to properly write the file';
+$lang['extension_file_not_allowed'] = 'Unable to upload the file extension is not allowed';
+$lang['destination_path_missing'] = 'The destination folder does not exist';
+$lang['multi_upload_error'] = 'You can not upload more than one file at once';
+$lang['min_size_error'] = 'The file does not reach the required size';
+$lang['max_size_error'] = 'The file has exceeded the available size';
+$lang['filetype_error'] = 'The file type is not valid';
+$lang['extension_error'] = 'The extension is not valid';
+
+/* EOF: ./lib/upload/locale/en.php */
