1029debf507c549a6c92157a251e4508cdbd4b20
Alvaro Cabrera <pateketrueke@gmail.com>
2011-08-14 10:13:09 -0500

    Change output order; cli fix

--- library/assets/views/raise.txt.php
+++ library/assets/views/raise.txt.php
@@ -2,36 +2,9 @@
 
 ob_start();
 
-echo ents($message, TRUE), "\n";
+echo "\n\nIncluded files\n--------------\n";  
+dump(get_included_files(), TRUE);
 
-echo "\nApplication\n-----------\n";  
-dump(array(
-  'user' => "$user@$host",
-  'route' => $route,
-  'params' => params(),
-  'bootstrap' => APP_LOADER,
-), TRUE);
-
-echo "\n\nConfig\n------\n";  
-dump(config(), TRUE);
-
-if (isset($env))
-{
-  echo "\n\nEnvironment\n-----------\n";  
-  dump($env, TRUE);
-}
-
-if (isset($global))
-{
-  echo "\n\nGlobals\n-------\n";  
-  dump($global, TRUE);
-}
-
-if (isset($headers))
-{
-  echo "\n\nHeaders\n-------\n";  
-  dump($headers, TRUE);
-}
 
 if (isset($backtrace))
 {
@@ -40,8 +13,40 @@ if (isset($backtrace))
 }
 
 
-echo "\n\nIncluded files\n--------------\n";  
-dump(get_included_files(), TRUE);
+if (isset($global))
+{
+  echo "\n\nGlobals\n-------\n";  
+  dump($global, TRUE);
+}
+
+
+if (isset($headers))
+{
+  echo "\n\nHeaders\n-------\n";  
+  dump($headers, TRUE);
+}
+
+
+echo "\n\nConfig\n------\n";  
+dump(config(), TRUE);
+
+
+if (isset($env))
+{
+  echo "\n\nEnvironment\n-----------\n";  
+  dump($env, TRUE);
+}
+
+
+echo "\n\nApplication\n-----------\n";  
+dump(array(
+  'user' => "$user@$host",
+  'route' => $route,
+  'params' => params(),
+  'bootstrap' => APP_LOADER,
+), TRUE);
+
+echo "\n\nError\n-----\n", ents($message, TRUE);
 
 $content = preg_replace('/^/m', '  ', ob_get_clean());
 
