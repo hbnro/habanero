14ccbc7568cd5ef41d75c728c9de56361da2f3ae
Alvaro Cabrera <pateketrueke@gmail.com>
2011-08-15 23:42:30 -0500

    Add other options from helpers

--- library/config.php
+++ library/config.php
@@ -7,8 +7,12 @@ $config['language'] = 'es_MX';
 $config['import_path'] = getcwd();
 $config['temporary_files'] = '/tmp';
 $config['timezone'] = 'America/Mexico_City';
+$config['encoding'] = 'UTF-8';
 $config['rewrite'] = FALSE;
 $config['suffix'] = '';
-$config['query'] = '';
+$config['cache'] = 'php';
+$config['perms'] = 0777;
+$config['query'] = FALSE;
+$config['pdo'] = FALSE;
 
 /* EOF: ./config.php */
