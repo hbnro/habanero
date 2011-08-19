4d443cd9ad8571467add9e6794fbd07e4e8e9ee6
Alvaro Cabrera <pateketrueke@gmail.com>
2011-08-18 23:20:02 -0500

    Separate components

---
+++ library/i18n/functions.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Translation core functions
+ */
+
+/**
+ * Dynamic shortcode alias
+ *
+ * @param  mixed Input string|...
+ * @return mixed
+ */
+function ln($input)
+{
+  $args  = func_get_args();
+  
+  if (is_array($input))
+  {
+    foreach ($input as $key => $value)
+    {
+      $args[0]     = $value;
+      $input[$key] = call_user_func_array(__FUNCTION__, $args);
+    }
+  }
+  else
+  {
+    $callback = is_num($input) ? 'pluralize' : 'translate';
+    $input    = call_user_func_array(array('i18n', $callback), $args);
+  }
+  
+  return $input;
+}
+
+/* EOF: ./lib/i18n/functions.php */
