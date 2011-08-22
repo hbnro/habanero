f8d6861ef5ddd228d512bfc4a55a62efe581b643
Alvaro Cabrera <pateketrueke@gmail.com>
2011-08-21 20:34:46 -0500

    Added taml library

---
+++ library/lib/tetl/taml/initialize.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * TAML initialization
+ */
+
+/**
+ * @ignore
+ */
+
+require __DIR__.DS.'system'.EXT;
+
+load_path(__DIR__.DS.'locale', 'taml');
+
+/* EOF: ./lib/taml/initialize.php */
