be344051f1c2d9ab565d5ba8d6d484e62fba0d5c
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-08-23 11:15:34 -0500

    Added styles to raise; short_open_tag off by default

--- library/assets/views/raise.html.php
+++ library/assets/views/raise.html.php
@@ -1,29 +1,45 @@
 <!doctype html>
 <html>
   <head>
-    <meta charset="<?= CHARSET; ?>">
-    <title><?= ln('error'); ?></title>
-    <style><!-- TODO: stylish --></style>
+    <meta charset="<?php echo CHARSET; ?>">
+    <title><?php echo ln('error'); ?></title>
+    <style>
+    p {
+      padding: .5em;
+      font-size: .9em;
+      background: #ededed;
+      font-family: Palatino, "Palatino Linotype", "Hoefler Text", Times, "Times New Roman", serif;
+    }
+    pre {
+      font-family: Monaco, "Bitstream Vera Sans Mono", "Lucida Console", Terminal, monospace;
+      font-size: .8em;
+    }
+    h3 {
+      border-bottom: 3px dotted #dedede;
+      font-family: "Lucida Sans", "Lucida Grande", Lucida, sans-serif;
+      font-size: 1.3em;
+    }
+    </style>
   </head>
   <body>
-    <pre><?= ents($message, TRUE); ?></pre>
-    <h3><?= ln('application'); ?></h3>
+    <p><?php echo ents($message, TRUE); ?></p>
+    <h3><?php echo ln('application'); ?></h3>
     <?php dump(array(
       'user' => "$user@$host",
       'route' => $route,
       'params' => params(),
       'bootstrap' => APP_LOADER,
     ), TRUE); ?>
-    <h3><?= ln('configuration'); ?></h3>
+    <h3><?php echo ln('configuration'); ?></h3>
     <?php dump(config(), TRUE); ?>
-    <?php if (isset($env)) { ?><h3><?= ln('environment'); ?></h3>
+    <?php if (isset($env)) { ?><h3><?php echo ln('environment'); ?></h3>
     <?php dump($env, TRUE); } ?>
-    <?php if (isset($headers)) { ?><h3><?= ln('headers'); ?></h3>
+    <?php if (isset($headers)) { ?><h3><?php echo ln('headers'); ?></h3>
     <?php dump($headers, TRUE); } ?>
-    <?php if (isset($backtrace)) { ?><h3><?= ln('backtrace'); ?></h3>
-    <?php dump($backtrace, TRUE); } ?><h3><?= ln('includes'); ?></h3>
+    <?php if (isset($backtrace)) { ?><h3><?php echo ln('backtrace'); ?></h3>
+    <?php dump($backtrace, TRUE); } ?><h3><?php echo ln('includes'); ?></h3>
     <?php dump(get_included_files(), TRUE); ?>
-    <p><?php echo ticks(defined('BEGIN') ? BEGIN : 0); ?>s</p>
+    <p>&mdash; <?php echo ticks(defined('BEGIN') ? BEGIN : 0); ?>s</p>
   </body>
 </html>
 <?php exit;
