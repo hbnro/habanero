f1cc65697233c6cd50d03d028c63f70b1f5bea57
Alvaro Cabrera <pateketrueke@gmail.com>
2011-08-24 02:21:43 -0500

    White-space issues

--- example/css_taml.php
+++ example/css_taml.php
@@ -4,15 +4,15 @@ require dirname(__DIR__).'/library/initialize.php';
 
 run(function()
 {
-  
+
   uses('tetl/css');
   uses('tetl/taml');
   uses('tetl/router');
-  
+
   css::setup('path', __DIR__);
-  
+
   $css = css::parse(__DIR__.DS.'assets'.DS.'sample.css');
-  
+
   taml::render(__DIR__.DS.'assets'.DS.'sample.taml', compact('css'));
-  
+
 });
