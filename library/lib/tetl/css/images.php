595c8f30bad1667837fc4b945323abf82a04a8d7
Alvaro Cabrera <pateketrueke@gmail.com>
2011-08-23 22:11:44 -0500

    Staging develop

--- library/lib/tetl/css/images.php
+++ library/lib/tetl/css/images.php
@@ -13,38 +13,38 @@
 css::implement('image', function($path)
 {
   static $cache = array();
-  
-  
+
+
   if (isset($cache[$path]))
   {
     return $cache[$path];
   }
-  
-  
+
+
   $out = array(
     'width' => 'auto',
     'height' => 'auto',
     'url' => "url!($path)",
   );
-  
-  
+
+
   $img_file = css::path($path);
-  
+
   if (is_file($img_file))
   {
     $test = getimagesize($img_file);
-    
+
     $out['width']  = "$test[0]px";
     $out['height'] = "$test[1]px";
-    
+
     $out['data']   = 'data:image/' . str_replace('jpg', 'jpeg', ext($img_file));
     $out['data']  .= ';base64,' . base64_encode(read($img_file));
-    
-    $out['url']    = sprintf('url!(%s)', url_to(css::path($path)));
+
+    $out['url']    = url_to(css::path($path));
   }
-  
+
   $cache[$path] = $out;
-  
+
   return $out;
 });
 
