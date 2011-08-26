582ca460a7ba8d98a18807489a1292474f79458f
Alvaro Cabrera <pateketrueke@gmail.com>
2011-08-26 01:18:28 -0500

    Added css_taml example

--- example/css_taml.php
+++ example/css_taml.php
@@ -1,5 +1,23 @@
 <?php
 
+if ( ! empty($_GET['grid']))
+{
+  $width = $_GET['grid'];
+
+  $image = imagecreatetruecolor($width * 2,1);
+
+  $one   = imagecolorallocate($image, 230, 230, 230);
+  $two   = imagecolorallocate($image, 210, 210, 210);
+
+  imagefill($image, 0, 0, $one);
+  imagerectangle($image, 0, 0, $width, 1, $two);
+
+  header('Content-Type: image/png');
+  imagepng($image);
+  exit;
+}
+
+
 require dirname(__DIR__).'/library/initialize.php';
 
 run(function()
