c94ea9bcef44748c2508a3309b663dab207baaec
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-08-31 12:18:56 -0500

    Deleted mvc related examples

--- example/sandbox/app/views/module/action.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-echo '<a href="', link_to('/'), '">&laquo; ', $var, '</a><br>';
-echo ticks(BEGIN);
--- example/sandbox/config/application.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$config['mvc']['controller_path'] = dirname(__DIR__).'/app/controller';
-$config['mvc']['view_path'] = dirname(__DIR__).'/app/views';
