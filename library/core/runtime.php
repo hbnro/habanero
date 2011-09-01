21ac696dece25de5b931640c562a917e4b4425d7
Alvaro Cabrera <pateketrueke@gmail.com>
2011-09-01 04:24:38 -0500

    Moved raise logic; be relax

--- library/core/runtime.php
+++ library/core/runtime.php
@@ -109,98 +109,13 @@ function render($content, $partial = FALSE, array $params = array())
  */
 function raise($message)
 {
-  $var   = array();
-  $args  = func_get_args();
-  $trace = array_slice(debug_backtrace(), 1);
-
-
-  // finalize opened buffers
-  while (ob_get_level())
-  {
-    ob_end_clean();
-  }
-
-  if ( ! empty($GLOBALS['--raise-message']))
-  {// this could be used in fatal error scenarios
-    $message = $GLOBALS['--raise-message'];
-    unset($GLOBALS['--raise-message']);
-  }
-
-
-  foreach ($trace as $i => $on)
-  {
-    $type   = ! empty($on['type']) ? $on['type'] : '';
-    $system = ! empty($on['file']) && strstr($on['file'], LIB) ?: FALSE;
-    $prefix = ! empty($on['object']) ? get_class($on['object']) : ( ! empty($on['class']) ? $on['class'] : '');
-
-    $call   = $prefix . $type . $on['function'];
-
-
-    // - app source
-    // + system source
-    // ~ unknown source
-
-    $format_str = ($true = ! empty($on['file'])) ? '%s %s#%d %s()' : '~ %4$s';
-    $format_val = sprintf($format_str, is_true($system) ? '+' : '-', $true ? $on['file'] : '', $true ? $on['line'] : '', $call);
-
-    $trace[$i]  = $format_val;
-  }
-
-  $var['message']   = dump($message);
-  $var['backtrace'] = array_reverse($trace);
-  $var['route']     = IS_CLI ? @array_shift($_SERVER['argv']) : value($_SERVER, 'REQUEST_URI');
-
-  if ( ! IS_CLI)
-  {
-    // raw headers
-    foreach (headers_list() as $one)
-    {
-      list($key, $val) = explode(':', $one);
-
-      $var['headers'][$key] = trim($val);
-    }
-  }
-
-
-  // system info
-  $var['host'] = php_uname('n');
-  $var['user'] = 'Unknown';
-
-  foreach (array('USER', 'LOGNAME', 'USERNAME', 'APACHE_RUN_USER') as $key)
-  {
-    if ($one = @getenv($key))
-    {
-      $var['user'] = $one;
-    }
-  }
-
-
-  // environment info
-  $var['env'] = $_SERVER;
-
-  foreach (array('PATH_TRANSLATED', 'DOCUMENT_ROOT', 'REQUEST_TIME', 'argc', 'argv') as $key)
-  {
-    if (isset($var['env'][$key]))
-    {
-      unset($var['env'][$key]);
-    }
-  }
-
-  // received headers
-  foreach ((array) $var['env'] as $key => $val)
-  {
-    if (preg_match('/^(?:PHP|HTTP|SCRIPT)/', $key))
-    {
-      if ( ! IS_CLI && (substr($key, 0, 5) === 'HTTP_'))
-      {// there is no need to use request object since are not required
-        $var['received'][camelcase(strtolower(substr($key, 5)), TRUE, '-')] = $val;
-      }
-      unset($var['env'][$key]);
-    }
+  if (is_closure($message))
+  {// TODO: there is another way?
+    return bootstrap::implement('raise', $message);
   }
 
   // invoke custom handler
-  bootstrap::raise($var);
+  bootstrap::raise($message);
 }
 
 
