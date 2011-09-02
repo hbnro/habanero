8e1f4bbf554f19fa2d28231bf3ecf8f5a8133a2b
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-09-02 18:03:04 -0500

    Added view basic support

---
+++ library/lib/tetl/mvc/view.php
@@ -0,0 +1,32 @@
+<?php
+
+class View extends prototype
+{
+ // TODO: huh?
+
+  final public static function load($file, array $vars = array())
+  {
+    $extension = ext($file);
+
+    switch ($extension)
+    {
+      case 'taml';
+        import('tetl/taml');
+        return taml::render($view_file, $vars);
+      break;
+      case 'php';
+      case 'phtml';
+        return render(array(
+          'partial' => $file,
+          'locals' => $vars,
+        ));
+      break;
+      default;
+        die("Unknown extension: $extension");
+      break;
+    }
+  }
+
+}
+
+/* EOF: ./lib/tetl/mvc/view.php */
