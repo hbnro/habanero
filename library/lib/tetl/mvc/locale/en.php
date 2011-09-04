78df807ce7866ec6b31acbb7e9ab39f957b1dccc
Alvaro Cabrera <pateketrueke@gmail.com>
2011-09-04 08:55:41 -0500

    Added language strings

--- library/lib/tetl/mvc/locale/en.php
+++ library/lib/tetl/mvc/locale/en.php
@@ -4,4 +4,13 @@
  * MVC english strings
  */
 
+$lang['controller_missing'] = 'Controller %{name} missing';
+$lang['class_missing'] = 'Conntroller class %{controller} missing';
+$lang['action_missing'] = 'Controller action  %{controller}#%{action} missing';
+$lang['helper_missing'] = 'Controller helper %{name} missing';
+$lang['view_missing'] = 'Controller view  for %{controller}#%{action} is missing';
+$lang['layout_missing'] = 'Layout %{name} missing';
+$lang['undefined_property'] = 'Undefined ṕroperty %{class}.%{name}';
+$lang['primary_key_missing'] = 'Primary key for %{model} is missing';
+
 /* EOF: ./lib/tetl/mvc/locale/en.php */
--- library/lib/tetl/mvc/locale/es.php
+++ library/lib/tetl/mvc/locale/es.php
@@ -4,4 +4,13 @@
  * MVC spanish strings
  */
 
+$lang['controller_missing'] = 'No existe el controlador %{name}';
+$lang['class_missing'] = 'No existe la clase %{controller}';
+$lang['action_missing'] = 'No existe la acción %{controller}#%{action}';
+$lang['helper_missing'] = 'No existe el asistente %{name}';
+$lang['view_missing'] = 'No existe la vista %{controller}#%{action}';
+$lang['layout_missing'] = 'No existe la plantilla %{name}';
+$lang['undefined_property'] = 'La propiedad %{class}.%{name} no está definida';
+$lang['primary_key_missing'] = 'La clave primaria de %{model} no existe';
+
 /* EOF: ./lib/tetl/mvc/locale/en.php */
