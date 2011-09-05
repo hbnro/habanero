aa06ad94952f5d0a8f34c1207005b96a5f7e06cd
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-09-05 17:00:22 -0500

    Added html5reset templates

--- console/core/assets/skeleton/app/initialize.php
+++ console/core/assets/skeleton/app/initialize.php
@@ -9,5 +9,47 @@ import('tetl/mvc');
 
 run(function()
 {
+  // handle assets
+  get('/_/*path', function()
+  {
+    $res_file = dirname(__DIR__).DS.'assets'.DS.params('path');
+    $res_file = strtr($res_file, '/', DS);
+
+    if ( ! is_file($res_file))
+    {
+      status(404);
+      exit;
+    }
+
+    // TODO: implement cache, gzip or anything better?
+    $extension = ext($res_file);
+
+    switch ($extension)
+    {
+      case 'jpeg';
+      case 'jpg';
+      case 'png';
+      case 'gif';
+        $mime_type = "image/$extension";
+      break;
+      case 'ico';
+        $mime_type = 'image/x-icon';
+      break;
+      case 'css';
+        $mime_type = 'text/css';
+      break;
+      case 'js';
+        $mime_type = 'text/x-javascript';
+      break;
+      default;
+        $mime_type = mime($res_file);
+      break;
+    }
+
+    header("Content-Type: $mime_type");
+    readfile($res_file);
+    exit;
+  });
+
   require __DIR__.DS.'routes'.EXT;
 });
