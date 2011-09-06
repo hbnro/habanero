e575fe9f62f95a25bcb4ab69e7405136d90808d5
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-09-05 19:27:25 -0500

    Added spanish strings

--- library/lib/tetl/console/locale/es.php
+++ library/lib/tetl/console/locale/es.php
@@ -1,8 +1,12 @@
 <?php
+
 /**
- *
+ * Console spanish strings
  */
 
-$lang['press_any_key'] = '';
+$lang['press_any_key'] = 'Presiona una tecla';
+$lang['choose_one_option'] = 'Elige una opci—n';
+$lang['unknown_option'] = 'Opci—n desconocida';
+$lang['available_commands'] = 'Comandos disponibles';
 
 /* EOF: ./lib/console/locale/es.php */
