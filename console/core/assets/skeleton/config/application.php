d93bcc5aeebca48844e0c8627e65885a69880f33
Alvaro Cabrera <pateketrueke@gmail.com>
2011-09-07 22:30:44 -0500

    Added environment default

--- console/core/assets/skeleton/config/application.php
+++ console/core/assets/skeleton/config/application.php
@@ -4,3 +4,5 @@ $config['mvc']['controllers_path'] = dirname(__DIR__).DS.'app'.DS.'controllers';
 $config['mvc']['helpers_path'] = dirname(__DIR__).DS.'app'.DS.'helpers';
 $config['mvc']['models_path'] = dirname(__DIR__).DS.'app'.DS.'models';
 $config['mvc']['views_path'] = dirname(__DIR__).DS.'app'.DS.'views';
+
+$config['environment'] = 'development';
