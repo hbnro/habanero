928d2737aa28e083e7e49ecb3851c0a68468b05c
Alvaro Cabrera <pateketrueke@gmail.com>
2011-09-17 02:57:04 -0500

    Added string for missing filters

--- library/lib/tetl/taml/locale/en.php
+++ library/lib/tetl/taml/locale/en.php
@@ -4,6 +4,7 @@
  * English taml strings
  */
 
+$lang['unknown_filter'] = 'Unknown filter :%{name}';
 $lang['error_file'] = 'Error on file %{name}#%{line} "%{text}"';
 $lang['error_line'] = 'Error on line %{line}, "%{text}"';
 $lang['bad_space'] = 'Indentation invalid';
--- library/lib/tetl/taml/locale/es.php
+++ library/lib/tetl/taml/locale/es.php
@@ -3,7 +3,8 @@
 /**
  * Spanish taml strings
  */
- 
+
+$lang['unknown_filter'] = 'Filtro :%{name} desconocido';
 $lang['error_file'] = 'Error en el archivo %{name}#%{line} "%{text}"';
 $lang['error_line'] = 'Error en la linea %{line}, "%{text}"';
 $lang['bad_space'] = 'El espaciado es incorrecto';
