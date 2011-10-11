ee07a60db14b44540c609ff9d9b75cdc1f4897e3
Alvaro Cabrera <pateketrueke@gmail.com>
2011-10-11 18:57:48 -0500

    Improved DI

--- stack/library/app/base/model/initialize.php
+++ stack/library/app/base/model/initialize.php
@@ -5,26 +5,11 @@ rescue(function($class)
   /**
     * @ignore
     */
+  $model_file = CWD.DS.'app'.DS.'models'.DS.$class.EXT;
+  $driver_file = __DIR__.DS.'drivers'.DS.$class.EXT;
 
-  switch ($class)
-  {
-    case 'dbmodel';
-      import('tetl/db');
-    case 'mongdel';
-      require __DIR__.DS.'drivers'.DS.$class.EXT;
-    break;
-    case 'model';
-      require __DIR__.DS.'system'.EXT;
-    break;
-    default;
-      $model_file = CWD.DS.'app'.DS.'models'.DS.$class.EXT;
-
-      if (is_file($model_file))
-      {
-        require $model_file;
-      }
-    break;
-  }
+  is_file($driver_file) && require $driver_file;
+  is_file($model_file) && require $model_file;
   /**#@-*/
 });
 
