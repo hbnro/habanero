f2474a53d42d10b55d38bc0a9b084b6dea2259ae
Alvaro Cabrera <pateketrueke@gmail.com>
2011-10-18 00:38:28 -0500

    Added install.sh magic

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+if [ "$(uname -s)" != "Linux" ]; then
+  echo "Linux only, apologies."
+else
+  if [ "$(whoami)" != "root" ]; then
+    echo "Please run script as root (or sudo)."
+  else
+    TETL="$HOME/.tetlphp"
+    mkdir -p "$TETL/tmp"
+    cd "$TETL/tmp"
+
+
+    echo "Downloading latest version."
+
+    curl -L http://tinyurl.com/gettetl | tar xzf -
+
+    TMPPATH="$TETL/tmp/$(ls $TETL/tmp)"
+
+
+    echo "Configuring at $TETL"
+
+    cd $TMPPATH
+    cp -R . $TETL
+    rm -rf "$TETL/tmp"
+
+
+    cd $HOME
+
+
+    echo "Creating symlink."
+
+    SYMLINK="/usr/local/bin/tetl"
+
+    if [ -h $SYMLINK ] || [ -e $SYMLINK ]; then
+      unlink $SYMLINK
+    fi
+
+    ln -s "$TETL/console/bin" $SYMLINK
+
+    echo "Done."
+  fi
+fi
