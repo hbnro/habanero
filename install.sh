bb1f9ad51f27570af87a416c6bcabf1e349e4cb3
Alvaro Cabrera <pateketrueke@gmail.com>
2011-10-20 01:03:52 -0500

    Moved away everything; better organization

--- install.sh
+++ install.sh
@@ -1,45 +1,63 @@
 #!/bin/sh
 
-if [ "$(uname -s)" != "Linux" ]; then
-  echo "Linux only, apologies."
-else
-  if [ "$(whoami)" != "root" ]; then
-    echo "Please run script as root (or sudo)."
-  else
-    TETL="$HOME/.tetlphp"
-    mkdir -p "$TETL/tmp"
-    cd "$TETL/tmp"
-
-
-    echo "Downloading latest version."
-
-    curl -L http://tinyurl.com/gettetl | tar xzf -
-
-    TMPPATH="$TETL/tmp/$(ls $TETL/tmp)"
-
-
-    echo "Configuring at $TETL"
-
-    cd "$TMPPATH"
-    cp -R . "$TETL"
-    rm -rf "$TETL/tmp"
-
-
-    cd $HOME
-
-
-    echo "Creating symlink."
-
-    SYMLINK="/usr/local/bin/tetl"
-
-    if [ -h "$SYMLINK" ] || [ -d "$SYMLINK" ]; then
-      echo "$SYMLINK"
-    fi
-
-    ln -s "$TETL/console/bin" $SYMLINK
-
-    echo "Installing."
-    sudo $SYMLINK --install
-    echo "Done."
-  fi
+if [ `whoami` != "root" ]; then
+  echo "Please run script as root (with sudo)"
+  exit 1
 fi
+
+if [ $SUDO_USER = "root" ]; then
+  echo "Please run script with sudo"
+  exit 1
+fi
+
+
+if [ ! -d "/Users" ]; then
+  TETL="$HOME/.tetlphp"
+else
+  TETL="$HOME/Library/PHP/tetlphp"
+fi
+
+
+URL="http://tinyurl.com/gettetl"
+mkdir -p "$TETL/tmp"
+cd "$TETL/tmp"
+
+
+echo "Downloading latest version"
+
+curl -L $URL | tar xzf -
+
+TMPPATH="$TETL/tmp/$(ls $TETL/tmp)"
+
+
+echo "Configuring at $TETL"
+
+cd "$TMPPATH"
+cp -R . "$TETL"
+rm -rf "$TETL/tmp"
+
+
+cd $HOME
+
+
+BINPATH="/usr/local/bin"
+
+if [ ! -d "$BINPATH" ]; then
+  BINPATH="/usr/bin"
+fi
+
+
+echo "Creating symlink"
+
+SYMLINK="$BINPATH/tetl"
+
+if [ -h "$SYMLINK" ] || [ -e "$SYMLINK" ]; then
+  unlink "$SYMLINK"
+fi
+
+ln -s "$TETL/console/bin" $SYMLINK
+
+
+echo "Installing"
+exec $SYMLINK --install
+echo "Done"
--- uninstall.sh
+++ uninstall.sh
@@ -1,30 +1,46 @@
 #!/bin/sh
 
-if [ "$(uname -s)" != "Linux" ]; then
-  echo "Linux only, apologies."
-else
-  if [ "$(whoami)" != "root" ]; then
-    echo "Please run script as root (or sudo)."
-  else
-    echo ":) Good bye dude."
-
-    TETL="$HOME/.tetlphp"
-
-    if [ -e "$TETL" ]; then
-      rm -rf "$TETL"
-    fi
-
-    echo "Removing symlink."
-
-    SYMLINK="/usr/local/bin/tetl"
-
-    if [ -h "$SYMLINK" ] || [ -e "$SYMLINK" ]; then
-      unlink "$SYMLINK"
-    fi
-
-    echo "Uninstalling."
-    sudo $SYMLINK --uninstall
-
-    echo "Done."
-  fi
+if [ `whoami` != "root" ]; then
+  echo "Please run script as root (with sudo)"
+  exit 1
 fi
+
+if [ $SUDO_USER = "root" ]; then
+  echo "Please run script with sudo"
+  exit 1
+fi
+
+
+if [ ! -d "/Users" ]; then
+  TETL="$HOME/.tetlphp"
+else
+  TETL="$HOME/Library/PHP/tetlphp"
+fi
+
+
+echo "Removing framework files"
+
+if [ -e "$TETL" ]; then
+  rm -rf "$TETL"
+fi
+
+
+BINPATH="/usr/local/bin"
+
+if [ ! -d "$BINPATH" ]; then
+  BINPATH="/usr/bin"
+fi
+
+
+echo "Removing symlink"
+
+SYMLINK="$BINPATH/tetl"
+
+if [ -h "$SYMLINK" ] || [ -e "$SYMLINK" ]; then
+  unlink "$SYMLINK"
+fi
+
+
+echo "Uninstalling"
+exec $SYMLINK --uninstall
+echo "Done"
