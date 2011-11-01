8a218f5f113d31d1d22d489cb28d935708b97b54
Alvaro Cabrera <pateketrueke@gmail.com>
2011-11-01 10:10:46 -0600

    Updated cross-browser vhost creation

--- stack/mods/_vhost.php
+++ stack/mods/_vhost.php
@@ -1,15 +1,24 @@
 <?php
 
-// Ubuntu only
+info("Verifying vhost availability");
+
 
 $base_path = CWD;
 $base_name = basename($base_path);
 
-info("Verifying vhost availability");
+$test = array(
+  '/etc/apache2/sites-available',
+  '/private/etc/apache2/virtualhosts',
+);
 
+foreach ($test as $vhost_path) {
+  if (is_dir($vhost_path)) {
+    break;
+  }
+}
 
-$vhost_path = '/etc/apache2/sites-available';
 $vhost_file = "$vhost_path/$base_name";
+
 
 if (cli::flag('remove')) {
   if ( ! is_file($vhost_file)) {
@@ -19,16 +28,21 @@ if (cli::flag('remove')) {
     unlink($vhost_file);
 
     sleep(1);
-    system("a2dissite $base_name");
-    system('/etc/init.d/apache2 restart');
+
+
+    !! `whereis a2dissite` && system("a2dissite $base_name");
+
+    $apache_bin = '/etc/init.d/apache2';
+
+    ! is_file($apache_bin) && $apache_bin = 'apachectl';
+
+    system("$apache_bin restart");
   }
 } elseif ( ! cli::flag('force') && is_file($vhost_file)) {
   error("Already exists $vhost_file");
 } else {
-  $public    = cli::flag('root') ?: 'public';
-  $public    = $public ? '/' . trim($public, '/') : '';
-  $doc_root  = "$base_path$public";
-  $scheme    = <<<XML
+  $doc_root = "$base_path/public";
+  $scheme   = <<<XML
 <VirtualHost *:80>
   ServerName   $base_name.dev
   DocumentRoot $doc_root
@@ -48,8 +62,15 @@ XML;
   success("Writing $vhost_file");
 
   sleep(1);
-  system("a2ensite $base_name");
-  system('/etc/init.d/apache2 restart');
+
+
+  !! `whereis a2ensite` && system("a2ensite $base_name");
+
+  $apache_bin = '/etc/init.d/apache2';
+
+  ! is_file($apache_bin) && $apache_bin = 'apachectl';
+
+  system("$apache_bin restart");
 }
 
 
