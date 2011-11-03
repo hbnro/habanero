2ce232f009845a2f614ec968ab5d4340fdac209d
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-11-03 17:48:35 -0600

    Testing through Mac OS X 10.6

--- stack/mods/_vhost.php
+++ stack/mods/_vhost.php
@@ -11,44 +11,44 @@ $test = array(
   '/private/etc/apache2/virtualhosts',
 );
 
-foreach ($test as $vhost_path) {
-  if (is_dir($vhost_path)) {
+foreach ($test as $one) {
+  if (is_dir($one)) {
+    $vhost_path = $one;
     break;
   }
 }
 
+// TODO: two types of vhost-install -> via vhost-path/vhost-conf-file
+// -> /private/etc/apache2/extra/httpd-vhosts.conf (Mac OS X 10.6)
+
 if (empty($vhost_path)) {
   error('Not found a suitable vhost path on your system!');
-  exit;
-}
-
-
-$vhost_file = "$vhost_path/$base_name";
-
-
-if (cli::flag('remove')) {
-  if ( ! is_file($vhost_file)) {
-    error("Not exists $vhost_file");
-  } else {
-    notice("Removing $vhost_file");
-    unlink($vhost_file);
-
-    sleep(1);
-
-
-    !! `whereis a2dissite` && system("a2dissite $base_name");
-
-    $apache_bin = '/etc/init.d/apache2';
-
-    ! is_file($apache_bin) && $apache_bin = 'apachectl';
-
-    system("$apache_bin restart");
-  }
-} elseif ( ! cli::flag('force') && is_file($vhost_file)) {
-  error("Already exists $vhost_file");
 } else {
-  $doc_root = "$base_path/public";
-  $scheme   = <<<XML
+  $vhost_file = "$vhost_path/$base_name";
+
+  if (cli::flag('remove')) {
+    if ( ! is_file($vhost_file)) {
+      error("Not exists $vhost_file");
+    } else {
+      notice("Removing $vhost_file");
+      unlink($vhost_file);
+
+      sleep(1);
+
+
+      !! `whereis a2dissite` && system("a2dissite $base_name");
+
+      $apache_bin = '/etc/init.d/apache2';
+
+      ! is_file($apache_bin) && $apache_bin = 'apachectl';
+
+      system("$apache_bin restart");
+    }
+  } elseif ( ! cli::flag('force') && is_file($vhost_file)) {
+    error("Already exists $vhost_file");
+  } else {
+    $doc_root = "$base_path/public";
+    $scheme   = <<<XML
 <VirtualHost *:80>
   ServerName   $base_name.dev
   DocumentRoot $doc_root
@@ -64,41 +64,42 @@ if (cli::flag('remove')) {
 XML;
 
 
-  write($vhost_file, $scheme);
-  success("Writing $vhost_file");
+    write($vhost_file, $scheme);
+    success("Writing $vhost_file");
 
-  sleep(1);
-
-
-  !! `whereis a2ensite` && system("a2ensite $base_name");
-
-  $apache_bin = '/etc/init.d/apache2';
-
-  ! is_file($apache_bin) && $apache_bin = 'apachectl';
-
-  system("$apache_bin restart");
-}
+    sleep(1);
 
 
-info('Verifying hosts file');
+    !! `whereis a2ensite` && system("a2ensite $base_name");
 
-$hosts_file = '/etc/hosts';
-$config     = trim(read($hosts_file)) . "\n";
-$text       = "127.0.0.1\t$base_name.dev ##\n";
+    $apache_bin = '/etc/init.d/apache2';
+
+    ! is_file($apache_bin) && $apache_bin = 'apachectl';
+
+    system("$apache_bin restart");
+  }
 
 
-if (cli::flag('force remove')) {
-  $config = str_replace($text, '', $config);
-  $config = str_replace($base_name, '', $config);
-}
+  info('Verifying hosts file');
 
-$modified = filesize($hosts_file) <> strlen($config);
+  $hosts_file = '/etc/hosts';
+  $config     = trim(read($hosts_file)) . "\n";
+  $text       = "127.0.0.1\t$base_name.dev ##\n";
 
-if (( ! cli::flag('remove') OR $modified) && ! strpos($config, $base_name)) {
-  success("Updating $hosts_file");
-  write($hosts_file, $config . (cli::flag('remove') ? '' : $text));
-} else {
-  notice('Nothing to update');
+
+  if (cli::flag('force remove')) {
+    $config = str_replace($text, '', $config);
+    $config = str_replace($base_name, '', $config);
+  }
+
+  $modified = filesize($hosts_file) <> strlen($config);
+
+  if (( ! cli::flag('remove') OR $modified) && ! strpos($config, $base_name)) {
+    success("Updating $hosts_file");
+    write($hosts_file, $config . (cli::flag('remove') ? '' : $text));
+  } else {
+    notice('Nothing to update');
+  }
 }
 
 bold('Done');
