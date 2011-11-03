f3a123200f1780d377af0bce14b88cbc03bda029
Alvaro Cabrera <pateketrueke@gmail.com>
2011-11-02 21:17:45 -0600

    Improvements on responding behavior

--- stack/library/application/scripts/binding.php
+++ stack/library/application/scripts/binding.php
@@ -57,29 +57,31 @@ request::implement('dispatch', function (array $params = array())
     /**#@-*/
 
     $class_name::defined('init') && $class_name::init();
-    $class_name::$action();
 
-    $view = partial::load(CWD.DS.'app'.DS.'views'.DS.$controller.DS.$action, (array) $class_name::$view);
+    if ($test = $class_name::$action()) {
+      @list($status, $view, $headers) = $test;
+      $class_name::$response = compact('status', 'headers');
+    } else {
+      $view = partial::load(CWD.DS.'app'.DS.'views'.DS.$controller.DS.$action, (array) $class_name::$view);
 
-    if ( ! is_false($class_name::$layout)) {
-      $class_name::$head []= tag('meta', array('name' => 'csrf-token', 'content' => TOKEN));
-      $class_name::$head []= tag('link', array('rel' => 'stylesheet', 'href' => url_for('/all.css')));
+      if ( ! is_false($class_name::$layout)) {
+        $class_name::$head []= tag('meta', array('name' => 'csrf-token', 'content' => TOKEN));
+        $class_name::$head []= tag('link', array('rel' => 'stylesheet', 'href' => url_for('/all.css')));
 
-      $layout_file = CWD.DS.'app'.DS.'views'.DS.'layouts'.DS.$class_name::$layout;
+        $layout_file = CWD.DS.'app'.DS.'views'.DS.'layouts'.DS.$class_name::$layout;
 
-      assets::inline(tag('script', array('src' => url_for('/all.js'))), 'body');
+        assets::inline(tag('script', array('src' => url_for('/all.js'))), 'body');
 
-      $view = partial::load($layout_file, array(
-        'body' => $view,
-        'head' => join("\n", $class_name::$head),
-        'title' => $class_name::$title,
-      ));
+        $view = partial::load($layout_file, array(
+          'body' => $view,
+          'head' => join("\n", $class_name::$head),
+          'title' => $class_name::$title,
+        ));
+      }
     }
 
     $output = $class_name::$response;
-
     $output['output'] = $view;
-
     response($output);
   }
 });
