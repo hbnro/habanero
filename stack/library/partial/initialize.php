095128e7ffe509476ec35e9d093f8587c9133f18
Alvaro Cabrera <pateketrueke@gmail.com>
2011-11-05 15:15:16 -0600

    Added fefault php render

--- stack/library/partial/initialize.php
+++ stack/library/partial/initialize.php
@@ -4,3 +4,11 @@ i18n::load_path(__DIR__.DS.'locale', 'partial');
 
 require __DIR__.DS.'functions'.EXT;
 require __DIR__.DS.'partial'.EXT;
+
+
+// render callback
+partial::register('php', function ($file, array $vars = array()) {
+  return render($file, TRUE, array(
+      'locals' => $vars,
+    ));
+});
