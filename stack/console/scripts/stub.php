8d1546a584670db6075c9c267f6e005d8622d91f
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-11-07 14:11:10 -0600

    Updated stub installation

--- stack/console/scripts/stub.php
+++ stack/console/scripts/stub.php
@@ -8,16 +8,29 @@ is_dir($tetl_path) && unfile($tetl_path, '*', DIR_RECURSIVE | DIR_EMPTY);
 mkpath($tetl_path);
 
 
-$framework = LIB;
-$library   = dirname(LIB).DS.'library';
-$stack     = dirname(LIB).DS.'stack';
+$libs = array();
+$base = dirname(LIB);
+$stub = read(CWD.DS.'Stubfile');
 
+preg_match_all('/\s*-\s*(\S+)/m', $stub, $matches);
 
-// TODO: allow to customize the stub-installation
+! empty($matches[1]) && $libs = $matches[1];
 
-foreach (array('framework', 'library', 'stack') as $path) {
-  success(ln('copying_stub_path', array('name' => $path, 'path' => $tetl_path)));
-  cpfiles($$path, $tetl_path.DS.$path, '*', TRUE);
+// TODO: allow + to copy full paths? i.e. + stack/console
+success(ln('copying_stub_path', array('name' => 'framework', 'path' => $tetl_path)));
+cpfiles(LIB, $tetl_path.DS.'framework', '*', TRUE);
+
+$stub_path = $tetl_path.DS.'library';
+
+foreach (config('import_path') as $path) {
+  foreach (dir2arr($path, '*', DIR_RECURSIVE | DIR_MAP) as $one) {
+    $import = str_replace($path.DS, '', $one);
+
+    if (in_array($import, $libs)) {
+      success(ln('copying_stub_path', array('name' => $import, 'path' => $stub_path)));
+      cpfiles($one, $stub_path.DS.$import, '*', TRUE);
+    }
+  }
 }
 
 done();
