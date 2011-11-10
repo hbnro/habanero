a1880dd989fd7c4d80c7d3fe127db0e834f5733a
Alvaro Cabrera <pateketrueke@gmail.com>
2011-11-10 00:09:44 -0600

    Added console generator, without help?

---
+++ library/a_record/generator.php
@@ -0,0 +1,47 @@
+<?php
+
+require __DIR__.DS.'initialize'.EXT;
+
+app_generator::usage(ln('ar.generator_title'), ln('ar.generator_usage'));
+
+app_generator::alias('console', 'c');
+
+
+
+// a record console
+app_generator::implement('console', function () {
+  import('a_record');
+
+  $scope = new stdClass;
+
+  cli::main(function ()
+    use($scope) {
+
+    $test = cli::readln('>>> ');
+
+    if (in_array($test, array('exit', 'quit'))) {
+      cli::quit();
+    } else {
+      // TODO: implement more expressions!
+      if (preg_match('/^(\w+\.\w+)(?:\s+(.*?))?$/', $test, $match)) {
+        $test = explode('.', $match[1]);
+        $args = explode(' ', ! empty($match[2]) ? trim($match[2]) : '');
+
+        $out  = @$test[0]::apply($test[1], $args);
+
+        pretty(function ()
+          use($out) {
+          if (is_array($out)) {
+            foreach ($out as $one) {
+              printf(">>> $one\n");
+            }
+          } else {
+            printf(">>> $out\n");
+          }
+        });
+      }
+    }
+  });
+});
+
+/* EOF: ./library/a_record/generator.php */
