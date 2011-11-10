c9479e3cf12fdbd2a40aa76ea72f3fc4ee61c275
Alvaro Cabrera <pateketrueke@gmail.com>
2011-11-09 23:56:53 -0600

    Format changed a bit

--- library/taml/locale/en.php
+++ library/taml/locale/en.php
@@ -5,8 +5,8 @@
  */
 
 $lang['unknown_filter'] = 'Unknown filter :%{name}';
-$lang['error_file'] = 'Error on file %{name}#%{line} "%{text}"';
-$lang['error_line'] = 'Error on line %{line}, "%{text}"';
+$lang['error_file'] = 'Error on file %{name}#%{line} `%{text}`';
+$lang['error_line'] = 'Error on line %{line} `%{text}`';
 $lang['bad_space'] = 'Indentation invalid';
 
 /* EOF: ./library/taml/locale/en.php */
--- library/taml/locale/es.php
+++ library/taml/locale/es.php
@@ -5,8 +5,8 @@
  */
 
 $lang['unknown_filter'] = 'Filtro :%{name} desconocido';
-$lang['error_file'] = 'Error en el archivo %{name}#%{line} "%{text}"';
-$lang['error_line'] = 'Error en la linea %{line}, "%{text}"';
+$lang['error_file'] = 'Error en el archivo %{name}#%{line} `%{text}`';
+$lang['error_line'] = 'Error en la linea %{line} `%{text}`';
 $lang['bad_space'] = 'El espaciado es incorrecto';
 
 /* EOF: ./library/taml/locale/es.php */
