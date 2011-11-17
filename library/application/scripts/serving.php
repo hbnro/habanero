da7c054355e9432c4c38bf9d24150f04eeabba4c
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-11-17 13:18:08 -0600

    Improve chained assets compiling

--- library/application/scripts/serving.php
+++ library/application/scripts/serving.php
@@ -1,13 +1,11 @@
 <?php
 
 // filters
-assets::compile('css', function ($file) {
-  return partial::render($file);
-});
-
-assets::compile('js', function ($file) {
-  return partial::render($file);
-});
+foreach (array('php', 'css', 'js') as $one) {
+  assets::compile($one, function ($file) {
+    return partial::render($file);
+  });
+}
 
 
 $type      = params('type');
@@ -30,15 +28,21 @@ switch (option('environment')) {
   default;
     $test = preg_replace_callback('/\s+\*=\s+(\S+)/m', function ($match)
       use($base_path, $type) {
-      assets::append($base_path.DS.$type.DS."$match[1].$type");
+        $test_file = $base_path.DS.$type.DS."$match[1].$type";
+
+        @list($path, $name) = array(dirname($test_file), basename($test_file));
+
+        assets::append(findfile($path, $name, FALSE, 1), $type);
     }, read($base_file));
 
     $test = preg_replace('/\/\*[*\s]*?\*\//s', '', $test);
 
     write($out_file, assets::$type($test));
+    echo read($out_file);
   break;
 }
 
-redirect(path_to($type.DS.basename($out_file)));
+exit;
+//redirect(path_to($type.DS.basename($out_file)));
 
 /* EOF: ./library/application/scripts/serving.php */
