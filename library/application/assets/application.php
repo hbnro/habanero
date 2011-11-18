8af5a70ba96e4ace62628f5bcda32af66aecbe75
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-11-18 14:34:53 -0600

    Added the default database configuration

--- library/application/assets/application.php
+++ library/application/assets/application.php
@@ -1,5 +1,5 @@
 <?php
 
 $config['environment'] = strpos(value($_SERVER, 'HTTP_HOST'), '.com') ? 'production' : 'development';
-
+$config['database'] = 'sqlite:'.getcwd().DS.'database'.DS.'db.sqlite';
 $config['rewrite'] = 0;
