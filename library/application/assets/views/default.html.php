ac3ae35fb37333158db744374d9b36b9634c0d64
Alvaro Cabrera <alvaro@outofthebox.mx>
2011-12-26 13:31:13 -0600

    Updated jquery version

--- library/application/assets/views/default.html.php
+++ library/application/assets/views/default.html.php
@@ -29,8 +29,8 @@
     </footer>
   </div>
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write("<script src='<?php echo assets::url_for('jquery-1.5.1.min.js'); ?>'>\x3C/script>")</script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write("<script src='<?php echo assets::url_for('jquery-1.7.1.min.js'); ?>'>\x3C/script>")</script>
 <?php echo assets::after(); ?>
 
 <!--
