e47d297e4b0950efac6944c61cf6a81331f8cb5f
Alvaro Cabrera <pateketrueke@gmail.com>
2012-01-04 00:12:04 -0600

    Indeed, temporary_files option is not useful

--- library/application/assets/config.php
+++ library/application/assets/config.php
@@ -3,7 +3,6 @@
 # $config['rewrite'] = 1;
 # $config['language'] = 'en';
 # $config['timezone'] = 'UTC';
-# $config['temporary_files'] = __DIR__.DS.'tmp';
 
 require __DIR__.DS.'config'.DS.'application'.EXT;
 require __DIR__.DS.'config'.DS.'environments'.DS.APP_ENV.EXT;
