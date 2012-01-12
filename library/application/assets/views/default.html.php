210aef92b8205d2e44fa35841778987d1b178eac
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-01-12 16:41:08 -0600

    Updated default layout to Modernizr 2.0.6

--- library/application/assets/views/default.html.php
+++ library/application/assets/views/default.html.php
@@ -33,21 +33,19 @@
     <script>window.jQuery || document.write("<script src='<?php echo assets::url_for('jquery-1.7.1.min.js'); ?>'>\x3C/script>")</script>
 <?php echo assets::after(); ?>
 
-<!--
+  <!--
     <script>
-
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-XXXXXX-XX']);
-      _gaq.push(['_trackPageview']);
-
-      (function () {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
+      window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
+      Modernizr.load({
+        load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
+      });
     </script>
--->
+  -->
+
+  <!--[if lt IE 7 ]>
+    <script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+  <![endif]-->
 
   </body>
 </html>
