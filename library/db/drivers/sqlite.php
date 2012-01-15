91ad3aed1d9f85ce6a742e5a813a24904f9c123a
Alvaro Cabrera <pateketrueke@gmail.com>
2012-01-15 13:37:53 -0600

    SQLite driver/scheme passes the specs

--- library/db/drivers/sqlite.php
+++ library/db/drivers/sqlite.php
@@ -8,96 +8,86 @@ if ( ! class_exists('SQLite3')) {
   raise(ln('extension_missing', array('name' => 'SQLite3')));
 }
 
-/**#@+
- * @ignore
- */
-define('RANDOM', 'RANDOM()');
-define('DB_DRIVER', 'SQLite3');
-/**#@-*/
+class sqlite extends sqlite_scheme {
 
+  private $last_query = NULL;
 
-sql::implement('connect', function () {
-  static $object = NULL;
-
-
-  if (is_null($object)) {
-    $parts   = func_get_arg(0);
-    $db_file = $parts['host'] . $parts['path'];
+  final public static function factory($params) {
+    $db_file = $params['host'] . $params['path'];
 
     if ( ! is_file($db_file) && ($db_file <> ':memory:')) {
       raise(ln('file_not_exists', array('name' => $db_file)));
     }
 
-    $object = new SQLite3($db_file);
-
-    $object->createfunction ('concat', function () {
+    $obj = new static;
+    $obj->res = new SQLite3($db_file);
+    $obj->res->createfunction('concat', function () {
       return implode(func_get_args(), '');
     });
 
-    $object->createfunction ('md5rev', function ($str) {
+    $obj->res->createfunction('md5rev', function ($str) {
       return strrev(md5($str));
     }, 1);
 
-    $object->createfunction ('mod', function ($a, $b) {
+    $obj->res->createfunction('mod', function ($a, $b) {
       return $a % $b;
     }, 2);
 
-    $object->createfunction ('md5', function ($str) {
+    $obj->res->createfunction('md5', function ($str) {
       return md5($str);
     }, 1);
 
-    $object->createfunction ('now', function () {
+    $obj->res->createfunction('now', function () {
       return time();
     }, 0);
+
+    return $obj;
   }
 
-  return $object;
-});
-
-sql::implement('version', function () {
-  $test = sql::connect()->version();
-
-  return $test['versionString'];
-});
-
-sql::implement('execute', function ($sql) {//FIX
-  sql::connect()->lastQuery = $sql;
-
-  return sql::connect()->query($sql);
-});
-
-sql::implement('escape', function ($test) {
-  return str_replace("'", "''", stripslashes($test));
-});
-
-sql::implement('error', function () {
-  return sql::connect()->lastErrorCode() ? sql::connect()->lastErrorMsg() : FALSE;
-});
-
-sql::implement('result', function ($res) {
-  return ($tmp = sql::fetch_assoc($res)) ? array_shift($tmp) : FALSE;
-});
-
-sql::implement('fetch_assoc', function ($res) {
-  return $res ? $res->fetchArray(SQLITE3_ASSOC) : FALSE;
-});
-
-sql::implement('fetch_object', function ($res) {
-  if ($res && $out = sql::fetch_assoc($res)) {//FIX
-    return (object) $out;
+  final public function version() {
+    $test = $this->res->version();
+    return $test['versionString'];
   }
-});
 
-sql::implement('count_rows', function ($res) {//FIX
-  return sql::result(sql::execute(sprintf('SELECT COUNT(*) FROM (%s)', sql::connect()->lastQuery)));
-});
+  final public function execute($sql) {//FIX
+    $this->last_query = $sql;
+    return @$this->res->query($sql);
+  }
 
-sql::implement('affected_rows', function () {
-  return sql::connect()->changes();
-});
+  final public function real_escape($test) {
+    return str_replace("'", "''", stripslashes($test));
+  }
 
-sql::implement('last_id', function () {
-  return sql::connect()->lastInsertRowID();
-});
+  final public function has_error() {
+    return $this->res->lastErrorCode() ? $this->res->lastErrorMsg() : FALSE;
+  }
+
+  final public function fetch_result($res) {
+    return ($tmp = $this->fetch_assoc($res)) ? array_shift($tmp) : FALSE;
+  }
+
+  final public function fetch_assoc($res) {
+    return $res ? $res->fetchArray(SQLITE3_ASSOC) : FALSE;
+  }
+
+  final public function fetch_object($res) {
+    if ($res && $out = $this->fetch_assoc($res)) {//FIX
+      return (object) $out;
+    }
+  }
+
+  final public function count_rows($res) {//FIX
+    $sql = sprintf('SELECT COUNT(*) FROM (%s)', $this->last_query);
+    return $this->last_query ? $this->fetch_result($this->execute($sql)) : FALSE;
+  }
+
+  final public function affected_rows() {
+    return $this->res->changes();
+  }
+
+  final public function last_inserted_id() {
+    return $this->res->lastInsertRowID();
+  }
+}
 
 /* EOF: ./library/db/drivers/sqlite.php */
