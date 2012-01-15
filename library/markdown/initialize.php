47e2936f5890830bdbbaa0964169a57459b65c59
Alvaro Cabrera <pateketrueke@gmail.com>
2012-01-14 22:54:02 -0600

    Removed taml filter support

--- library/markdown/initialize.php
+++ library/markdown/initialize.php
@@ -9,13 +9,6 @@ call_user_func(function() {
 	 */
 	require __DIR__.DS.'vendor'.DS.'markdown'.EXT;
 
-	// :markdown filter
-	if (class_exists('taml')) {
-		taml::shortcut('markdown', function ($args, $plain, $params) {
-	    return Markdown($plain);
-	  });
-	}
-
 	// allow for Markdown files
 	if (class_exists('partial')) {
 		partial::register(array('md', 'markdown'), function ($file, array $vars = array()) {
