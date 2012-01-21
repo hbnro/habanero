7e1cdbe340219e9c5df1d62e9dae668021630ff2
Alvaro Cabrera <pateketrueke@gmail.com>
2012-01-20 19:13:18 -0600

    Update default config for rsync

--- library/application/assets/tasks/rsync/initialize.php
+++ library/application/assets/tasks/rsync/initialize.php
@@ -12,28 +12,28 @@ class rsync_task extends prototype {
     $rsync = __DIR__;
     extract(static::$defs['dev']);
     notice(ln('rsync.default_deploy'));
-    system("rsync --dry-run -avzlC --exclude-from $rsync/exclude.txt --stats --progress --delete -e '{$ssh_transport_beta}' . {$ssh_user_beta}:{$remote_root_beta}");
+    system("rsync --dry-run -avz --exclude-from $rsync/exclude.txt --stats --progress --delete -e '{$ssh_transport_beta}' . {$ssh_user_beta}:{$remote_root_beta}");
   }
 
   final public static function dev() {
     $rsync = __DIR__;
     extract(static::$defs['dev']);
     notice(ln('rsync.development_deploy'));
-    system("rsync -avzlC --exclude-from $rsync/exclude.txt --stats --progress --delete -e '{$ssh_transport_beta}' . {$ssh_user_beta}:{$remote_root_beta}");
+    system("rsync -avz --exclude-from $rsync/exclude.txt --stats --progress --delete -e '{$ssh_transport_beta}' . {$ssh_user_beta}:{$remote_root_beta}");
   }
 
   final public static function prod() {
     $rsync = __DIR__;
     extract(static::$defs['prod']);
     notice(ln('rsync.production_deploy'));
-    system("rsync --dry-run -avzlC --exclude-from $rsync/exclude.txt --stats --progress --delete -e '{$ssh_transport}' . {$ssh_user}:{$remote_root}");
+    system("rsync --dry-run -avz --exclude-from $rsync/exclude.txt --stats --progress --delete -e '{$ssh_transport}' . {$ssh_user}:{$remote_root}");
   }
 
   final public static function deploy() {
     $rsync = __DIR__;
     extract(static::$defs['prod']);
     notice(ln('rsync.final_deploy'));
-    system("rsync -avlzC --exclude-from $rsync/exclude.txt --progress --stats --delete -e '{$ssh_transport}' . {$ssh_user}:{$remote_root}");
+    system("rsync -avzZ --exclude-from $rsync/exclude.txt --progress --stats --delete -e '{$ssh_transport}' . {$ssh_user}:{$remote_root}");
   }
 
 }
