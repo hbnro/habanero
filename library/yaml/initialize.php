2477a179eb103ac261b999a4d25210a0eb8bf039
Alvaro Cabrera <pateketrueke@gmail.com>
2012-01-21 17:13:29 -0600

    First spec; using spyc

---
+++ library/yaml/initialize.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * YAML library support
+ */
+
+call_user_func(function () {
+  /**#@+
+   * @ignore
+   */
+  require __DIR__.DS.'vendor'.DS.'spyc'.EXT;
+
+  if ( ! function_exists('yaml_parse')) {
+    function yaml_parse($text) {
+      return spyc_load($text);
+    }
+
+    function yaml_parse_url($link) {
+      return spyc_load(read($link));
+    }
+
+    function yaml_parse_file($text) {
+      return spyc_load_file($link);
+    }
+
+    function yaml_emit($data) {
+      return Spyc::YAMLDump($data);
+    }
+
+    function yaml_emit_file($file, $data) {
+      return write($file, Spyc::YAMLDump($data));
+    }
+  }
+  /**#@-*/
+});
+
+/* EOF: ./library/yaml/initialize.php */
