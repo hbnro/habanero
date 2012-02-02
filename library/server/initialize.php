986c8d328952995299a5ff705ad69b49104ba0c0
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-02-02 11:23:20 -0600

    Session initialization fixed

--- library/server/initialize.php
+++ library/server/initialize.php
@@ -120,63 +120,61 @@ return function ($bootstrap) {
 
   ignore_user_abort(FALSE);
 
+  // default session handler
+  // http://php.net/session_set_save_handler
+
+  if ( ! session_id()) {
+    session_set_save_handler(function () {
+      return TRUE;
+    }, function () {
+      return TRUE;
+    }, function ($id) {
+      return read(TMP.DS."sess_$id");
+    }, function ($id, $data) {
+      return write(TMP.DS."sess_$id", $data);
+    }, function ($id) {
+      return @unlink(TMP.DS."sess_$id");
+    }, function ($max) {
+      foreach (dir2arr(TMP, "sess_*") as $one) {
+        if ((@filemtime(TMP.DS.$one) + $max) < time()) {
+          @unlink(TMP.DS.$one);
+        }
+      }
+    });
+
+
+    // session conf
+    if ( ! request::is_local()) {
+      $host = server();
+      $host = is_ip($host) ? $host : ".$host";
+
+      session_set_cookie_params(86400, ROOT, $host);
+    }
+
+    session_name('--a-' . preg_replace('/\W/', '-', phpversion()));
+    // TODO: BTW with session_id()?
+    session_start();
+  }
+
+
+  // expires+hops
+  foreach ($_SESSION as $key => $val) {
+    if ( ! is_array($val) OR ! array_key_exists('value', $val)) {
+      continue;
+    }
+
+    if (isset($_SESSION[$key]['expires']) && (time() >= $val['expires'])) {
+      unset($_SESSION[$key]);
+    }
+
+    if (isset($_SESSION[$key]['hops']) && ($_SESSION[$key]['hops']-- <= 0)) {
+      unset($_SESSION[$key]);
+    }
+  }
+
   return function ()
     use($bootstrap) {
-
-    // default session handler
-    // http://php.net/session_set_save_handler
-
-    if ( ! session_id()) {
-      session_set_save_handler(function () {
-        return TRUE;
-      }, function () {
-        return TRUE;
-      }, function ($id) {
-        return read(TMP.DS."sess_$id");
-      }, function ($id, $data) {
-        return write(TMP.DS."sess_$id", $data);
-      }, function ($id) {
-        return @unlink(TMP.DS."sess_$id");
-      }, function ($max) {
-        foreach (dir2arr(TMP, "sess_*") as $one) {
-          if ((@filemtime(TMP.DS.$one) + $max) < time()) {
-            @unlink(TMP.DS.$one);
-          }
-        }
-      });
-
-
-      // session conf
-      if ( ! request::is_local()) {
-        $host = server();
-        $host = is_ip($host) ? $host : ".$host";
-
-        session_set_cookie_params(86400, ROOT, $host);
-      }
-
-      session_name('--a-' . preg_replace('/\W/', '-', phpversion()));
-      // TODO: BTW with session_id()?
-      session_start();
-    }
-
-
-    // expires+hops
-    foreach ($_SESSION as $key => $val) {
-      if ( ! is_array($val) OR ! array_key_exists('value', $val)) {
-        continue;
-      }
-
-      if (isset($_SESSION[$key]['expires']) && (time() >= $val['expires'])) {
-        unset($_SESSION[$key]);
-      }
-
-      if (isset($_SESSION[$key]['hops']) && ($_SESSION[$key]['hops']-- <= 0)) {
-        unset($_SESSION[$key]);
-      }
-    }
-
-    $bootstrap();
-    routing::execute();
+    routing::execute($bootstrap());
   };
 };
 
