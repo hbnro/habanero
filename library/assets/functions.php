8d15facdc535d9b920a2a963bde3ce51b7fa990c
Alvaro Cabrera <pateketrueke@gmail.com>
2012-02-09 01:13:22 -0600

    Improve rules; slacking off...

--- library/assets/functions.php
+++ library/assets/functions.php
@@ -1,13 +1,27 @@
 <?php
 
+/**
+ * Asset helpers
+ */
+
+// includes js
 function javascript_for($name) {
   assets::prepend(tag('script', array('src' => assets::build($name, 'js'))), 'body');
 }
 
+// includes css
 function stylesheet_for($name) {
   assets::prepend(tag('link', array('rel' => 'stylesheet', 'href' => assets::build($name, 'css'))), 'head');
 }
 
+// includes security ;-)
 function csrf_meta_tag() {
   echo tag('meta', array('name' => 'csrf-token', 'content' => option('crsf_token')));
 }
+
+// good for routing at mockz!
+function asset_url($for) {
+  return path_to(assets::fetch($for), TRUE);
+}
+
+/* EOF: ./library&assets/functions.php */
