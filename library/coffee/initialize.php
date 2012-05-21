0ca4d6bb855c088a82c6fa5ab44a281810642a80
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-05-21 14:57:18 -0500

    Fallback implementation to CLI

--- library/coffee/initialize.php
+++ library/coffee/initialize.php
@@ -8,13 +8,11 @@ if (class_exists('partial', FALSE)) {
   /**
    * @ignore
    */
-  require __DIR__.DS.'vendor'.DS.'coffeescript'.EXT;
+  ! `coffee -v` && require __DIR__.DS.'vendor'.DS.'coffeescript'.EXT;
 
   // TODO: there is another solution?
   partial::register('coffee', function ($file, array $vars = array()) {
-    return Coffeescript\compile(read($file), array(
-      'bare' => TRUE,
-    ));
+    return coffee::compile($file);
   });
 }
 
@@ -33,11 +31,29 @@ class coffee
 {
   // file render
   final public static function compile($file) {
-    return Coffeescript\compile(read($file));
+    return static::parse(read($file));
   }
   // text parse
   final public static function parse($text) {
-    return Coffeescript\compile($text);
+    if ( !! `coffee -v`) {
+      $tmp_file = TMP.DS.uniqid('--coffee-input');
+      $out_file = TMP.DS.uniqid('--coffee-output');
+
+      write($tmp_file, $text);
+
+      system("coffee -sbp < $tmp_file > $out_file");
+
+      $out = read($out_file);
+
+      @unlink($tmp_file);
+      @unlink($out_file);
+
+      return $out;
+    } else {
+      return Coffeescript\compile($text, array(
+        'bare' => TRUE,
+      ));
+    }
   }
 }
 
