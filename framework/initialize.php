97f1fd76211d814a2364720e7f08ffebf43d3362
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-05-22 18:29:44 -0500

    Without initialization debug?

--- framework/initialize.php
+++ framework/initialize.php
@@ -144,17 +144,10 @@ call_user_func(function () {
     }
   });
 
-  // TODO: shutdown WAT?
-  register_shutdown_function(function () {
-    debug('EOX'); // EndOfExecution!
-  });
-
   // autoload
   foreach ((array) option('autoload') as $one) {
     $one && import($one);
   }
-
-  debug('---');
 });
 
 /* EOF: ./framework/initialize.php */
