dbba455250d83ef66f2318af950c3eb6ca9f4e39
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-06-12 16:29:33 -0500

    New implementation for --stub helper

--- stack/scripts/stub.php
+++ stack/scripts/stub.php
@@ -25,16 +25,17 @@ cpfiles(LIB, $tetl_path.DS.'framework', '*', TRUE);
 $stub_path = $tetl_path.DS.'library';
 $path      = dirname(LIB).DS.'library';
 
-if ($test = dir2arr($path, '*', DIR_RECURSIVE | DIR_MAP)) {
-  foreach ($test as $one) {
-    $import = str_replace($path.DS, '', $one);
+foreach ($libs as $one) {
+  $old = "$path/$one";
 
-    if (in_array(extn($import), $libs)) {
-      success(ln('copying_stub_path', array('name' => extn($import), 'path' => str_replace(APP_PATH, '.', $stub_path))));
-      is_dir($one) ? cpfiles($one, $stub_path.DS.$import, '*', TRUE) : copy($one, mkpath($stub_path).DS.$import);
-    }
-  }
+  ! is_dir($old) && $old .= EXT;
+
+  $import = str_replace($path.DS, '', $old);
+
+  success(ln('copying_stub_path', array('name' => extn($import), 'path' => str_replace(APP_PATH, '.', $stub_path))));
+  is_dir($old) ? cpfiles($old, $stub_path.DS.$import, '*', TRUE) : copy($old, mkpath($stub_path).DS.$import);
 }
+
 
 // compactize!
 if ( !! `php -v`) {
