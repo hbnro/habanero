8874f2cd56b224d77fc12f3b9ca91bb62811bd1d
Alvaro Cabrera <pateketrueke@gmail.com>
2012-06-14 22:29:59 -0500

    Avoid klass-overloading by namespace

--- framework/klass/prototype.php
+++ framework/klass/prototype.php
@@ -15,21 +15,22 @@ class prototype
   protected static $defs = array();
 
   // public function stack
-  private static $public = array();
+  protected static $public = array();
 
   // avoid constructor
-  private function __construct() {
+  protected function __construct() {
   }
 
   // public method callback
   final public static function __callStatic($method, $arguments = array()) {
-    if ( ! isset(self::$public[get_called_class()][$method])) {
-      if (static::defined('missing')) {
-        return static::missing($method, $arguments);
+    $klass = get_called_class();
+    if ( ! isset($klass::$public[$method])) {
+      if ($klass::defined('missing')) {
+        return $klass::missing($method, $arguments);
       }
-      raise(ln('method_missing', array('class' => get_called_class(), 'name' => $method)));
+      raise(ln('method_missing', array('class' => $klass, 'name' => $method)));
     }
-    return self::apply($method, $arguments);
+    return $klass::apply($method, $arguments);
   }
 
   /**#@-*/
@@ -42,7 +43,8 @@ class prototype
    * @param  mixed  Closure function
    */
   final public static function implement($method, Closure $lambda) {
-    self::$public[get_called_class()][$method] = $lambda;
+    $klass = get_called_class();
+    $klass::$public[$method] = $lambda;
   }
 
 
@@ -53,10 +55,11 @@ class prototype
    * @return boolean
    */
   final public static function defined($method) {
-    if (isset(self::$public[get_called_class()][$method])) {
+    $klass = get_called_class();
+    if (isset($klass::$public[$method])) {
       return TRUE;
     }
-    return method_exists(get_called_class(), $method);
+    return method_exists($klass, $method);
   }
 
 
@@ -66,7 +69,8 @@ class prototype
    * @return array
    */
   final public static function methods() {
-    return ! empty(self::$public[get_called_class()]) ? self::$public[get_called_class()] : array();
+    $klass = get_called_class();
+    return $klass::$public;
   }
 
 
@@ -78,10 +82,11 @@ class prototype
    * @return mixed
    */
   final public static function apply($method, array $args = array()) {
-    if (isset(self::$public[get_called_class()][$method])) {
-      return call_user_func_array(self::$public[get_called_class()][$method], $args);
+    $klass = get_called_class();
+    if (isset($klass::$public[$method])) {
+      return call_user_func_array($klass::$public[$method], $args);
     }
-    return call_user_func_array(get_called_class() . "::$method", $args);
+    return call_user_func_array("$klass::$method", $args);
   }
 
 
@@ -93,15 +98,16 @@ class prototype
    * @return void
    */
   final public static function config($key, $value = '') {
+    $klass = get_called_class();
     if (is_assoc($key)) {
-      static::$defs = array_merge(static::$defs, $key);
+      $klass::$defs = array_merge($klass::$defs, $key);
     } elseif (is_closure($key)) {
       $config = new stdClass;
       $key($config);
 
-      static::$defs = array_merge(static::$defs, (array) $config);
-    } elseif (array_key_exists($key, static::$defs)) {
-      static::$defs[$key] = $value;
+      $klass::$defs = array_merge($klass::$defs, (array) $config);
+    } elseif (array_key_exists($key, $klass::$defs)) {
+      $klass::$defs[$key] = $value;
     }
   }
 
@@ -114,7 +120,8 @@ class prototype
    * @return mixed
    */
   final public static function option($key, $default = FALSE) {
-    return ! empty(static::$defs[$key]) ? static::$defs[$key] : $default;
+    $klass = get_called_class();
+    return ! empty($klass::$defs[$key]) ? $klass::$defs[$key] : $default;
   }
 
 }
