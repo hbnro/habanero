aa5eccf6396d71652153e608f49a62797aa2449e
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-07-18 17:42:38 -0500

    Remove CLI support through shell; fix

--- library/coffee/initialize.php
+++ library/coffee/initialize.php
@@ -4,21 +4,17 @@
  * Coffee wrapper initialization
  */
 
-if ( ! `coffee -v`) {
-  $old = error_reporting();
-  error_reporting(0);
+$old = error_reporting();
+error_reporting(0);
 
-  /**
-   * @ignore
-   */
-  require __DIR__.DS.'vendor'.DS.'coffeescript'.EXT;
+/**
+ * @ignore
+ */
+require __DIR__.DS.'vendor'.DS.'coffeescript'.EXT;
 
-  error_reporting($old);
-}
+error_reporting($old);
 
 
-
-// TODO: there is another solution?
 partial::register('coffee', function ($context) {
   return coffee::parse($context);
 });
@@ -31,25 +27,9 @@ class coffee
 {
   // text parse
   final public static function parse($text) {
-    if ( !! `coffee -v`) {
-      $tmp_file = TMP.DS.uniqid('--coffee-input');
-      $out_file = TMP.DS.uniqid('--coffee-output');
-
-      write($tmp_file, $text);
-
-      system("coffee -sbp < $tmp_file > $out_file");
-
-      $out = read($out_file);
-
-      @unlink($tmp_file);
-      @unlink($out_file);
-
-      return $out;
-    } else {
-      return Coffeescript\compile($text, array(
-        'bare' => TRUE,
-      ));
-    }
+    return Coffeescript\compile($text, array(
+      'bare' => TRUE,
+    ));
   }
 }
 
