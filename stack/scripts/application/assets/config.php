cb3d2012c5d18081dc8b73a4d610c81fb4dc1c43
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-07-24 14:33:22 -0500

    Sample config keys

--- stack/scripts/application/assets/config.php
+++ stack/scripts/application/assets/config.php
@@ -3,6 +3,9 @@
 # $config['rewrite'] = 1;
 # $config['language'] = 'en';
 # $config['timezone'] = 'UTC';
+# $config['assets']['host'] = 'www.domain.tld';
+# $config['security']['csrf_expire'] = 300;
+# $config['database']['default'] = 'sqlite:'.APP_PATH.DS.'database'.DS.'db.sqlite';
 
 $config['cli_imports'] = array('chess', 'coffee', 'tamal');
 
