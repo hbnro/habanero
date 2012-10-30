71b03ab367c39e62980b8e82e970c52a12cba2dd
Alvaro Cabrera <pateketrueke@gmail.com>
2012-10-30 12:24:34 -0600

    Support for local vendors

--- sauce.php
+++ sauce.php
@@ -10,6 +10,19 @@
 // bundled full-stack
 $autoload = require __DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 
+// local vendors
+$vendor_dir = APP_PATH.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer';
+$classmap_file = $vendor_dir.DIRECTORY_SEPARATOR.'autoload_classmap.php';
+$namespaces_file = $vendor_dir.DIRECTORY_SEPARATOR.'autoload_namespaces.php';
+
+is_file($classmap_file) && $autoload->addClassMap(require $classmap_file);
+
+if (is_file($namespaces_file) && ($test = require $namespaces_file)) {
+  foreach ($test as $key => $val) {
+    $autoload->add($key, $val);
+  }
+}
+
 Sauce\Base::$autoload = $autoload;
 
 return $autoload;
