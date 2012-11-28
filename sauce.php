7e6a39ddb626efff52a0db4f569908fb3949183d
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-11-27 18:23:54 -0600

    Clear the initialization scope

--- sauce.php
+++ sauce.php
@@ -7,20 +7,22 @@
  * @link   https://github.com/pateketrueke/habanero
  */
 
-// bundled full-stack
-$autoload = require __DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+return call_user_func(function () {
+    // bundled full-stack
+    $autoload = require __DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 
-// local vendors
-$vendor_dir = APP_PATH.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer';
-$classmap_file = $vendor_dir.DIRECTORY_SEPARATOR.'autoload_classmap.php';
-$namespaces_file = $vendor_dir.DIRECTORY_SEPARATOR.'autoload_namespaces.php';
+    // local vendors
+    $vendor_dir = APP_PATH.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'composer';
+    $classmap_file = $vendor_dir.DIRECTORY_SEPARATOR.'autoload_classmap.php';
+    $namespaces_file = $vendor_dir.DIRECTORY_SEPARATOR.'autoload_namespaces.php';
 
-is_file($classmap_file) && $autoload->addClassMap(require $classmap_file);
+    is_file($classmap_file) && $autoload->addClassMap(require $classmap_file);
 
-if (is_file($namespaces_file) && ($test = require $namespaces_file)) {
-  foreach ($test as $key => $val) {
-    $autoload->add($key, $val);
-  }
-}
+    if (is_file($namespaces_file) && ($test = require $namespaces_file)) {
+      foreach ($test as $key => $val) {
+        $autoload->add($key, $val);
+      }
+    }
 
-return \Sauce\Base::$autoload = $autoload;
+    return \Sauce\Base::$autoload = $autoload;
+  });
