a98275bc363dfd1ebf54c835e9e19f4750d7848e
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-12-04 17:12:12 -0600

    Always double-check the illuminated path

--- bin/tasks/app/scripts/check_routes.php
+++ bin/tasks/app/scripts/check_routes.php
@@ -19,7 +19,7 @@ $match = 0;
 foreach ($set as $method => $sub) {
   foreach ($sub as $one) {
     foreach (array('to', 'path', 'match') as $i) {
-      strlen($one[$i]) > $$i && $$i = strlen($one[$i]);
+      isset($one[$i]) && (strlen($one[$i]) > $$i) && $$i = strlen($one[$i]);
     }
   }
 }
@@ -34,8 +34,9 @@ if ( ! empty($set)) {
     foreach ($set as $one) {
       @list($class, $action) = explode('#', $one['to']);
       $pad = str_pad($action, $to - strlen($class), ' ', STR_PAD_RIGHT);
+      $path = ! empty($one['path']) ? " {$one['path']}" : '';
 
-      $out []= sprintf("      \cgreen(%-{$match}s)\c  \cbrown($class)\c\clight_gray(#$pad)\c $one[path]", $one['match']);
+      $out []= sprintf("      \cgreen(%-{$match}s)\c  \cbrown($class)\c\clight_gray(#$pad)\c$path", $one['match']);
     }
     $out []= '';
   }
