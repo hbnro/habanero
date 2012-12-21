a0c0ad3614f267bb000405dab91d082a55613f51
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-12-21 14:04:28 -0600

    Cancel means 'show' action here

--- bin/tasks/db/assets/views/modify.php
+++ bin/tasks/db/assets/views/modify.php
@@ -4,7 +4,7 @@
 - stylesheet_for('app')
 
 section
-  = link_to('Cancel', url_for('<?php echo $base; ?>'))
+  = link_to('Cancel', url_for('show_<?php echo $name; ?>', array(':id' => $<?php echo $name; ?>-><?php echo $pk; ?>)))
   = partial('<?php echo $base; ?>/errors.php', compact('error'))
   = Form::put(url_for('update_<?php echo $name; ?>', array(':id' => $<?php echo $name; ?>-><?php echo $pk; ?>)), ~>
 <?php foreach ($fields as $key => $val) { ?>
