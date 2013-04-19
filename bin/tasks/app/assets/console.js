0722d889e6b091a573e901aa92619cd5db83b45e
Alvaro Cabrera <pateketrueke@gmail.com>
2013-04-18 20:23:26 -0500

    Move files and remove ol' scripts

--- bin/tasks/app/assets/console.js
+++
@@ -1,2 +0,0 @@
-// http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log=function(){log.history=log.history||[];log.history.push(arguments);if(this.console){console.log(Array.prototype.slice.call(arguments))}};
